2ed89e0edc57a98460ac16bb01827b88eba64fc8
Slava Chili <slavachili@gmail.com>
2022-07-06 14:42:24 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  new fullpage("#fullpage", {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: false,
+    anchors: ["firstPage", "secondPage"],
+    navigationTooltips: ["firstSlide", "secondSlide"],
+    navigation: true,
+    // showActiveTooltip: false,
+    responsiveWidth: 1200,
+    // scrollBar: true,
+  });
+});
